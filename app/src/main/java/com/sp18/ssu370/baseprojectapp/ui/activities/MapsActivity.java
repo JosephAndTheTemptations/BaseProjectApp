cd18834444ca8000f560b7b1d8c6f4d8d8e05a16
chavranek <havranek@sonoma.edu>
2018-04-25 12:09:54 -0700

    christianHermes

--- app/src/main/java/com/sp18/ssu370/baseprojectapp/ui/activities/MapsActivity.java
+++ app/src/main/java/com/sp18/ssu370/baseprojectapp/ui/activities/MapsActivity.java
@@ -1,12 +1,14 @@
 package com.sp18.ssu370.baseprojectapp.ui.activities;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.security.keystore.KeyNotYetValidException;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
@@ -15,9 +17,14 @@ import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -30,6 +37,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.sp18.ssu370.baseprojectapp.R;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 //public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -49,6 +57,8 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     private boolean startLocation = true;
 
+    private EditText mSearchText;
+
 
     LocationManager locationManager;
 
@@ -58,8 +68,56 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         //setContentView(R.layout.activity_maps);
         setContentView(R.layout.activity_set_destination);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        mSearchText = (EditText) findViewById(R.id.search_edit_text);
 
         getLocationPermission();
+
+    }
+
+    private void init(){
+        mSearchText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent keyEvent) {
+                if(actionId == EditorInfo.IME_ACTION_SEARCH
+                        || actionId == EditorInfo.IME_ACTION_DONE
+                        || keyEvent.getAction() == KeyEvent.ACTION_DOWN
+                        || keyEvent.getAction() == KeyEvent.KEYCODE_ENTER){
+
+                        geoLocate();
+                }
+                return false;
+            }
+        });
+        hideSoftKeyboard();
+    }
+
+    private void geoLocate(){
+        String searchString = mSearchText.getText().toString();
+
+        Geocoder geocoder = new Geocoder(MapsActivity.this);
+        List<Address> list = new ArrayList<>();
+        try{
+            list = geocoder.getFromLocationName(searchString, 1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if(list.size() > 0){
+            Address address = list.get(0);
+
+            //mMap.moveCamera(new LatLng(address.getLatitude(), address.getLongitude()), 10.2f,
+              //      address.getAddressLine(0));
+
+            LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+
+            String str = list.get(0).getAddressLine(0);
+            //str += list.get(0).getCountryName();
+           // hideSoftKeyboard();
+
+            mMap.addMarker(new MarkerOptions().position(latLng).title(str));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10.2f));
+            hideSoftKeyboard();
+
+        }
     }
 
     //private initMap new. This code was part of onCreate.
@@ -102,6 +160,8 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                         str += addressList.get(0).getCountryName();
                         mMap.addMarker(new MarkerOptions().position(latLng).title(str));
                         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10.2f));
+                        hideSoftKeyboard();
+
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
@@ -160,6 +220,8 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                         str += addressList.get(0).getCountryName();
                         mMap.addMarker(new MarkerOptions().position(latLng).title(str));
                         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10.2f));
+                        hideSoftKeyboard();
+
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
@@ -248,10 +310,8 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 return;
             }
             mMap.setMyLocationEnabled(true);
-            // Add a marker in Sydney and move the camera
-            //LatLng sydney = new LatLng(-34, 151);
-            //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-            //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+            init();
         }
 
     }
@@ -280,9 +340,22 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             String str = addressList.get(0).getLocality() + ",";
             str += addressList.get(0).getCountryName();
             LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+            //hideSoftKeyboard();
             mMap.addMarker(new MarkerOptions().position(latLng).title(str));
             mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+            hideSoftKeyboard();
+
         }
+    }
+
+    private void hideSoftKeyboard(){
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(mSearchText.getWindowToken(), 0);
+
+
+
+
+    //this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
     }
 
 }
