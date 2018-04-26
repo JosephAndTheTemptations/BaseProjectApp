4da5e5e85e3fb7ea901b009798c83eaf678b2b0e
josephcriseno <criseno@sonoma.edu>
2018-04-25 18:42:59 -0700

    recycler_toolbar

--- app/src/main/java/com/sp18/ssu370/baseprojectapp/ui/activities/MainActivity.java
+++ app/src/main/java/com/sp18/ssu370/baseprojectapp/ui/activities/MainActivity.java
@@ -1,14 +1,24 @@
 package com.sp18.ssu370.baseprojectapp.ui.activities;
 
+import android.content.Intent;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.sp18.ssu370.baseprojectapp.R;
+import com.sp18.ssu370.baseprojectapp.SettingsActivity;
 
 import java.util.ArrayList;
+
+import static com.sp18.ssu370.baseprojectapp.R.id.app_bar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,9 +32,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(app_bar);
+        setSupportActionBar(myToolbar);
         Log.d(TAG, "onCreate: started.");
 
         initImageBitmaps();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.map_settings:
+                Toast.makeText(getApplicationContext(), "Map", Toast.LENGTH_SHORT).show();
+                // Go to the map
+                break;
+            case R.id.action_settings:
+                // Go To Settings
+                Intent settings = new Intent(this, SettingsActivity.class);
+                startActivity(settings);
+                break;
+            case R.id.create_message:
+                Toast.makeText(getApplicationContext(), "Messages", Toast.LENGTH_SHORT).show();
+                // Go to messages
+                break;
+            default:
+                // Error
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private void initImageBitmaps(){
